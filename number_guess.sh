d3b24e9beb9b5ff534fae9ed04e59031a495ad90
antigx <tiago.mamedio.barbosa@gmail.com>
2023-07-17 15:01:29 +0000

    feat: add inserts to databse

--- number_guess.sh
+++ number_guess.sh
@@ -2,27 +2,58 @@
 PSQL="psql -X --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 
-echo "Enter yout username:"
-read USERNAME
+USER(){
+  echo -e "\nEnter your username:"
+  read USERNAME
 
-USER_DB=$($PSQL "select games_playeD, best_game from users where username = '$USERNAME'")
+  USER_DB=$($PSQL "select games_playeD, best_game from users where username = '$USERNAME'")
 
-if [[ -z $USER_DB ]]
-then
-  USERNAME_INSERT_RESULT=$($PSQL "insert into users(username) values('$USERNAME')")
-  echo "Welcome, $USERNAME! It looks like this is your first time here."
-fi
+  if [[ -z $USER_DB ]]
+  then
+    USERNAME_INSERT_RESULT=$($PSQL "insert into users(username) values('$USERNAME')")
+    echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+  else
+    IFS="|" read GAMES_PLAYED BEST_GAME <<< $USER_DB
+    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  fi
 
-IFS="|" read GAMES_PLAYED BEST_GAME <<< $USER_DB
+GAME
+}
 
-if [[ -z $GAMES_PLAYED ]]
-then
-  echo "Welcome, $USERNAME! It looks like this is your first time here."
-else
-  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-fi
+GAME(){
+  RANDOM_NUMBER="$(( RANDOM%1000 + 1 ))"
+  GUESSES=0
+  echo $RANDOM_NUMBER
+  echo -e "\nGuess the secret number between 1 and 1000:"
+  until [[ $GUESSED_NUMBER -eq RANDOM_NUMBER ]]
+  do
+    read GUESSED_NUMBER
+    (( GUESSES++ ))
+    if [[ $GUESSED_NUMBER > $RANDOM_NUMBER ]]
+    then
+      echo -e "\nIt's lower than that, guess again:"
+    else
+      echo -e "\nIt's higher than that, guess again:"
+    fi
+  done
+  INSERT
+  echo -e "\nYou guessed it in $GUESSES tries. The secret number was $RANDOM_NUMBER. Nice job!"
 
-echo $GAMES_PLAYED
-
+}
+INSERT() {
+    if [[ -z $USER_DB ]]
+    then 
+      UPDATE_PRIMEIRO_JOGO=$($PSQL "UPDATE users SET best_game = $GUESSES, games_played = 1 WHERE username='$USERNAME'")
+    else
+      if [[ $BEST_GAME > $GUESSES ]]
+      then
+        UPDATE_BEST_GAME=$($PSQL "update users set best_game = $GUESSES, games_played = games_played + 1 where username = '$USERNAME' ")
+      else
+        UPDATE_BEST_GAME=$($PSQL "update users set games_played = games_played + 1 where username = '$USERNAME' ")
+      fi
+    fi
+}
 
 RANDOM_NUMBER="$(( RANDOM%1000 + 1 ))"
+
+USER
